f7b6d42a97e39f114bce9ccfab8dd50ad9944bbe
AdarshSNair13 <adarshsnair13@gmail.com>
2024-03-31 15:30:45 +0530

    sitemap added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,13 +7,13 @@ const Hero = () => {
     <div className="bg-[#72e2ff] mt-12 py-12 flex items-center justify-center border-4 border-black drop-shadow-md rounded-2xl">
       <div className="flex flex-col items-center">
         <h1 className="font-bold drop-shadow-md-custom text-center text-5xl uppercase leading-tight md:text-8xl 2xl:text-9xl text-black">
-          Hi I'am
+          Hi I&apos;am
           <span className="bg-[#fe68c8] drop-shadow-solid-extend m-3 inline-block -rotate-3 rounded-full border border-black px-12 py-3 text-white">
             Adarsh
           </span>
         </h1>
         <p className="mt-8 w-full text-center md:w-3/4">
-          I'm a software engineer, <br />
+          I&apos;m a software engineer, <br />
           specializing in JavaScript, React, Next, TypeScript,Tailwind ,figma
           and Web Performance.
         </p>
